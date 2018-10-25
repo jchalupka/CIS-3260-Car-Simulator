ed107282a5ade9236c3d5a610496aa85781113f4
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2018-10-25 19:00:03 -0400

    init

---
+++ src/carsimulator/CarSimulator.java
@@ -0,0 +1,12 @@
+package carsimulator;
+
+public class CarSimulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
