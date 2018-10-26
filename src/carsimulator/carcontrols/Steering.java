00b1b0d736c870f6a4b863c8e0e7396120e729d5
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2018-10-25 20:36:00 -0400

    added logger

--- src/carsimulator/carcontrols/Steering.java
+++ src/carsimulator/carcontrols/Steering.java
@@ -1,6 +1,22 @@
 package carsimulator.carcontrols;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Steering {
+    private static final Logger logger = Logger.getLogger(Steering.class.getName());
     
+    /**
+     * turnLeft turns the car left.
+     */
+    public void turnLeft() {
+        logger.log(Level.INFO, "Turning left");
+    }
+    
+    /**
+     * turnRight turns the car right.
+     */
+    public void turnRight() {
+        
+    }
 }
