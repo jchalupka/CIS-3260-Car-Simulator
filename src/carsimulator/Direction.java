ad895b45e3f76be1a3a3f2af255cd05196792b2a
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2018-10-29 10:25:58 -0400

    more documenting changes with comments and adding log statements

--- src/carsimulator/Direction.java
+++ src/carsimulator/Direction.java
@@ -1,33 +1,68 @@
 package carsimulator;
 
-public class Direction {
+import java.util.logging.Logger;
 
+/**
+ * Direction models the direction of a car.
+ */
+public class Direction {
+    // Simple logger to log events
+    private static final Logger logger = Logger.getLogger(Car.class.getName());
+    
+    // The maximum and minimum amount a car can turn
+    // TODO we might not need this depending on how we handle steering, but
+    // I'm leaving it in to be more flexible.
     private static final double MAX_TURN_AMOUNT = Math.PI * 2;
     private static final double MIN_TURN_AMOUNT = 0;
     private static final double TURN_DIFFERENCE = MAX_TURN_AMOUNT - MIN_TURN_AMOUNT;
 
     private double val;
 
+    /**
+     * 
+     * @param curDirection 
+     */
     public Direction(double curDirection) {
         this.val = curDirection;
     }
 
+    /**
+     * getDirection gets the cars current direction.
+     * @return 
+     */
     public double getDirection() {
         return val;
     }
 
+    /**
+     * handleWrapAround keeps the direction variable between the max and min
+     * turn amount.
+     * @param val
+     * @return 
+     */
     public static double handleWrapAround(double val) {
         val = (((val - MIN_TURN_AMOUNT) % TURN_DIFFERENCE) + TURN_DIFFERENCE) % TURN_DIFFERENCE + MIN_TURN_AMOUNT;
         return val;
     }
 
+    /**
+     * increase handles increasing the current direction by turnAmount.
+     * @param turnAmount
+     * @return 
+     */
     public double increase(double turnAmount) {
+        
         this.val += turnAmount;
         this.val = handleWrapAround(this.val);
 
         return this.val;
     }
 
+    /**
+     * decrease handles decreasing the current direction by turnAmount.
+     * @param turnAmount
+     * @return 
+     */
     public double decrease(double turnAmount) {
         this.val -= turnAmount;
         this.val = handleWrapAround(this.val);
--- src/carsimulator/Gas.java
+++ src/carsimulator/Gas.java
@@ -1,39 +1,62 @@
 package carsimulator;
 
-public class Gas {
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+ * Gas is a model of gas within a car.
+ */
+public class Gas {
+    // Simple logger to log events
+    private static final Logger logger = Logger.getLogger(Car.class.getName());
+    
     private final int maxGasAmount;
     private int currentGasAmount;
     private final int siphonAmount;
 
+    /**
+     * @param maxGasAmount
+     * @param currentGasAmount 
+     */
     public Gas(int maxGasAmount, int currentGasAmount) {
         this.maxGasAmount = maxGasAmount;
         this.currentGasAmount = currentGasAmount;
         this.siphonAmount = 10;
+        
+        logger.log(Level.INFO, "Created new gas object", this);
     }
 
     public int getCurrentGasAmount() {
         return currentGasAmount;
     }
 
+    /**
+     * getSiphonAmount gets the siphon amount for the gas.
+     * @return the amount siphoned.
+     */
     public int getSiphonAmount() {
         return siphonAmount;
     }
 
     /**
+     * refuel refuels the gas.
      * Only refueling to max will be allowed.
      */
     public void refuel() {
+        logger.log(Level.INFO, "Refueling");
         this.currentGasAmount = this.maxGasAmount;
     }
 
     /**
+     * useGas makes a call to removeGas.
+     * 
      * Making this it's own function because I'm assuming it's going to have
      * special functionality later (talking to view).
      *
      * @param amount
      */
     public void useGas(int amount) {
+        logger.log(Level.INFO, "Decreased gas by", amount);
         // TODO send notification to view to update the fuel guage.
         this.removeGas(amount);
     }
@@ -42,11 +65,14 @@ public class Gas {
      * Removes fuel by the siphon amount.
      */
     public void siphon() {
+        logger.log(Level.INFO, "Siphoning gas");
         this.removeGas(siphonAmount);
     }
 
     /**
-     * Removes fuel (only if there is fuel to remove).
+     * removeGas removes fuel if there is fuel to remove.
+     * 
+     * @param amount 
      */
     private void removeGas(int amount) {
         this.currentGasAmount = Math.max(0, this.currentGasAmount - amount);
