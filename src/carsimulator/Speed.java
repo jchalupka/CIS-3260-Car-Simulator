6d5102f91d10c3eea04589cc2b70c2641ff4561a
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2018-10-30 18:04:23 -0400

    moved logic relevent to pedals to the pedal class and added speed getters and setters

--- src/carsimulator/Speed.java
+++ src/carsimulator/Speed.java
@@ -3,24 +3,54 @@ package carsimulator;
 import java.util.logging.Logger;
 
 public class Speed {
+
     // Simple logger to log events
     private static final Logger logger = Logger.getLogger(Car.class.getName());
+
+    private static final int MAX_SPEED = 100;
+    private static final int MIN_SPEED = 0;
     
+   
     private int speed;
-    
+
     /**
-     * Speed is the speed of the car
-     * @param speed 
+     * Speed is the speed of the car.
+     * 
+     * speed will be set to zero upon initialization.
      */
-    public Speed(int speed) {
-        this.speed = speed;
+    public Speed() {
+        this.speed = 0;
     }
-    
-    private void increaseSpeed(int speed_delta) {
-        this.speed += speed_delta;
+
+    public static int getMAX_SPEED() {
+        return MAX_SPEED;
     }
-    
-    private void decreaseSpeed(int speed_delta) {
-        this.speed -= speed_delta;
+
+    public static int getMIN_SPEED() {
+        return MIN_SPEED;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    /**
+     * increaseSpeed increases the current speed by speed_delta. The function
+     * also ensures the speed does not go above the maximum speed.
+     *
+     * @param speed_delta
+     */
+    public void increaseSpeed(int speed_delta) {
+        this.speed = Math.min(MAX_SPEED, this.speed + speed_delta);
+    }
+
+    /**
+     * decreaseSpeed decreases the current speed by speed_delta. The function
+     * also ensures the speed does not go below the minimum speed.
+     *
+     * @param speed_delta
+     */
+    public void decreaseSpeed(int speed_delta) {
+        this.speed -= Math.max(MIN_SPEED, this.speed + speed_delta);;
     }
 }
--- src/carsimulator/carcontrols/Pedals.java
+++ src/carsimulator/carcontrols/Pedals.java
@@ -1,5 +1,47 @@
 package carsimulator.carcontrols;
 
+import java.util.logging.Logger;
+import carsimulator.Speed;
+
 public class Pedals {
-    
+
+    // Simple logger to log events
+    private static final Logger logger = Logger.getLogger(Pedals.class.getName());
+
+    // TODO come back to these constants to see what feels right with the animation
+    private static final int NATURAL_DESCELERATION_SPEED = 1; // Just a dumb guess
+    private static final int BRAKE_DESCELERATION_SPEED = 4; // Just a dumb guess
+    private static final int ACCELERATION_SPEED = 2; // Just a dumb guess
+
+    private final Speed speed;
+
+    /**
+     * Speed is the speed of the car
+     *
+     * @param speed
+     */
+    public Pedals() {
+        this.speed = new Speed();
+    }
+
+    /**
+     * coast decreases speed as if coasting by NATURAL_DESCELERATION_SPEED
+     */
+    public void coast() {
+        this.speed.decreaseSpeed(NATURAL_DESCELERATION_SPEED);
+    }
+
+    /**
+     * brake decelerates the car by BRAKE_DESCELERATION_SPEED
+     */
+    public void brake() {
+        this.speed.decreaseSpeed(BRAKE_DESCELERATION_SPEED);
+    }
+
+    /**
+     * accelerate accelerates the car by ACCELERATION_SPEED
+     */
+    public void accelerate() {
+        this.speed.increaseSpeed(ACCELERATION_SPEED);
+    }
 }
