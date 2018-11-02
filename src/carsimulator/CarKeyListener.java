ac85ce03ee8d97e39c0b96de05324c4e87f2b8a3
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2018-11-02 11:54:14 -0400

    added CarKeyListener

---
+++ src/carsimulator/CarKeyListener.java
@@ -0,0 +1,27 @@
+package carsimulator;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+/**
+ * This will implement all of the key event listeners for the car (I think)
+ */
+public class CarKeyListener implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
