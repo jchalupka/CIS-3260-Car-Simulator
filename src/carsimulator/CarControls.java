7bab9cf8aca6a1c9c9ab66fc9981ca747dca792d
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2018-11-02 13:44:37 -0400

    gave CarControls all of the controller properties

--- src/carsimulator/CarControls.java
+++ src/carsimulator/CarControls.java
@@ -1,13 +1,35 @@
 package carsimulator;
 
+import carsimulator.carcontrols.*;
+
 public class CarControls {
 
     private CarKeyListener keyListener;
     private CarUI carUI;
+    
+    private final Steering steering;
+    private final GearBox gearbox;
+    private final Odometer odometer;
+    private final Pedals pedals;
+    private final InternalTemperatureControl internalTemperatureControl;
+    
+    
 
     public CarControls(CarUI carUI) {
         this.carUI = carUI;
         this.keyListener = new CarKeyListener(this.carUI);
+        
+        // TODO should probably be a car controls factory
+        // TODO Speed and Direction should be taken from the Car model. 
+        // Direction should really be taken from the Car model.
+        Direction direction = new Direction(0.0);
+        this.steering = new Steering(direction);
+        this.gearbox = new GearBox();
+        this.odometer = new Odometer();
+        // Speed should really be taken from the Car model.
+        Speed speed = new Speed();
+        this.pedals = new Pedals(speed);
+        this.internalTemperatureControl = new InternalTemperatureControl();
     }
     
     public static void main(String[] args) {
