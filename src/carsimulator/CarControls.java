ec884c2eb24be192af7e84f7bf560f399dc32e6d
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2018-11-02 14:13:10 -0400

    got the controller working

--- src/carsimulator/CarControls.java
+++ src/carsimulator/CarControls.java
@@ -7,17 +7,18 @@ public class CarControls {
     private CarKeyListener keyListener;
     private CarUI carUI;
     
-    private final Steering steering;
-    private final GearBox gearbox;
-    private final Odometer odometer;
-    private final Pedals pedals;
-    private final InternalTemperatureControl internalTemperatureControl;
-    
-    
+    public final Steering steering;
+    public final GearBox gearbox;
+    public final Odometer odometer;
+    public final Pedals pedals;
+    public final InternalTemperatureControl internalTemperatureControl;
 
     public CarControls(CarUI carUI) {
         this.carUI = carUI;
-        this.keyListener = new CarKeyListener(this.carUI);
+        
+        // TODO change the order of these args
+        // Note this shouldn't subclass because it's a has a not an is a
+        this.keyListener = new CarKeyListener(this.carUI, this);
         
         // TODO should probably be a car controls factory
         // TODO Speed and Direction should be taken from the Car model. 
--- src/carsimulator/CarKeyListener.java
+++ src/carsimulator/CarKeyListener.java
@@ -9,10 +9,14 @@ import java.awt.Frame;
  */
 public class CarKeyListener implements KeyListener {
 
-    public CarKeyListener(Frame frame) {
+    // Going to call the controller when actions are found.
+    private final CarControls controller;
+
+    public CarKeyListener(Frame frame, CarControls controller) {
         frame.addKeyListener(this);
+        this.controller = controller;
     }
-    
+
     /**
      * The types of action the user would like to perform on the car.
      */
@@ -55,37 +59,72 @@ public class CarKeyListener implements KeyListener {
         }
     }
 
+    private void handleActionType(ActionType action) {
+        // TODO hide the implementation from the handler
+        // e.g. this.controller.brake vs this.controller.pedals.brake.
+        // what is the correct way to do this?
+        switch (action) {
+            case ACCELERATE:
+                this.controller.pedals.accelerate();
+                break;
+            case BRAKE:
+                this.controller.pedals.brake();
+                break;
+            case TURN_LEFT:
+                this.controller.steering.turnLeft();
+                break;
+            case TURN_RIGHT:
+                this.controller.steering.turnRight();
+                break;
+        }
+    }
+
+    /**
+     * TODO what does this actually do? It seems that only keyPressed is called.
+     */
     @Override
     public void keyTyped(KeyEvent e) {
         ActionType action = ActionType.getActionType(e);
         if (action == ActionType.UNKNOWN) {
             return;
         }
-        
-        // Perform the appropriate action on the controller
-        System.out.println(action);
     }
 
+    /**
+     * I think this is the only event listener that we need TODO comment out the
+     * rest?
+     *
+     * @param e
+     */
     @Override
     public void keyPressed(KeyEvent e) {
         ActionType action = ActionType.getActionType(e);
         if (action == ActionType.UNKNOWN) {
             return;
         }
-        
+
         // Perform the appropriate action on the controller
-        System.out.println(action);
+        System.out.println("keyPressed " + action);
+        
+        // handle the action
+        // this should probably be done in the controller
+        this.handleActionType(action);
     }
 
+    /**
+     * TODO do we really care about this?
+     *
+     * @param e
+     */
     @Override
     public void keyReleased(KeyEvent e) {
         ActionType action = ActionType.getActionType(e);
         if (action == ActionType.UNKNOWN) {
             return;
         }
-        
+
         // Perform the appropriate action on the controller
-        System.out.println(action);
+        System.out.println("keyReleased " + action);
     }
 
     public static void main(String[] args) {
@@ -94,7 +133,9 @@ public class CarKeyListener implements KeyListener {
         frame.setVisible(true);
 
         // Give the CarKeyListener the frame
-        new CarKeyListener(frame);
+        
+        // Just commenting this out because I'm changing the CarKeyListener constructor right now and it's breaking.
+        // new CarKeyListener(frame);
 
     }
 
