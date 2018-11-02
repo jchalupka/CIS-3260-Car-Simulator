ca725e26d429fd1d539afff3d6f7ef9a56290dc5
Connor Campbell <ccampb21@uoguelph.ca>
2018-11-02 17:23:52 -0400

    Added in comments for methods

--- src/carsimulator/carcontrols/GearBox.java
+++ src/carsimulator/carcontrols/GearBox.java
@@ -14,20 +14,36 @@ public class GearBox {
     }
     private Gear state;
     
+    /**
+     * GearBox has one variable, state, which is an enum for indicating the
+     * current gear. This variable is dependent on the gas class.
+     * @param gas 
+     */
     public GearBox(Gas gas) {
         this.gas = gas;
         this.state = Gear.Park;
     }
     
+    /**
+     * A method that returns the current Gear
+     * @return enum Gear which indicates the car's current gear
+     */
     public Gear getCurrentGear() {
         return this.state;
     }
     
+    /**
+     * Sets the state of the car to park
+     */
     public void setPark() {
         this.state = Gear.Park;
         LOGGER.log(Level.INFO, "Gear is in Park.");
     }
     
+    /**
+     * Checks the that the current gas amount is greater than 0
+     * @return a boolean that is true is the gas amount is greater than 0
+     */
     public boolean hasGas() {
         return(this.gas.getCurrentGasAmount() > 0);
     }
@@ -39,6 +55,10 @@ public class GearBox {
 //        }
 //    }
     
+    /**
+     * Sets the state of the car to drive if there is gas and the previous
+     * state was park
+     */
     public void setDrive() {
         if (this.state == Gear.Park && this.hasGas()) {
             LOGGER.log(Level.INFO, "Gear shifted to drive.");
@@ -46,6 +66,10 @@ public class GearBox {
         }
     }
     
+    /**
+     * Sets the state of the car to drive if there is gas and the previous
+     * state was park
+     */
     public void setReverse() {
         if (this.state == Gear.Park && this.hasGas()) {
             LOGGER.log(Level.INFO, "Gear shifted to reverse");
