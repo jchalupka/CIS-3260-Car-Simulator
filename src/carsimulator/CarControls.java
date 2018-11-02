23f1f3413cf82b8e1668f34aa665159f9badb1f4
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2018-11-02 13:33:05 -0400

    made CarUI extend frame and gave CarControls main The car controls will be the point where CarUI connects This makes more sense I think...

--- src/carsimulator/CarControls.java
+++ src/carsimulator/CarControls.java
@@ -1,5 +1,19 @@
 package carsimulator;
 
 public class CarControls {
+
+    private CarKeyListener keyListener;
+    private CarUI carUI;
+
+    public CarControls(CarUI carUI) {
+        this.carUI = carUI;
+        this.keyListener = new CarKeyListener(this.carUI);
+    }
     
+    public static void main(String[] args) {
+        CarUI carUI = new CarUI();
+        new CarControls(carUI);
+        carUI.setVisible(true);
+    }
+
 }
