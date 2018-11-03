e271a9cb86ce73471585e04580eb34820b1d8eb5
Justin Braham <justin@Justins-MacBook-Pro.local>
2018-11-03 12:05:13 -0400

    Changed location to Point var

--- src/carsimulator/Car.java
+++ src/carsimulator/Car.java
@@ -2,6 +2,7 @@ package carsimulator;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.awt.Point;
 
 /**
  * Car is data model of the car.
@@ -10,7 +11,7 @@ public class Car {
     // Simple logger to log events
     private static final Logger logger = Logger.getLogger(Car.class.getName());
     
-    private Location location;
+    private Point location;
     private Direction direction;
     private int speed;
     private Gas gas;
@@ -24,6 +25,7 @@ public class Car {
         // TODO add the rest of the variables here as their class is implemented
         this.direction = direction;
         this.gas = gas;
+        this.location = new Point(47, 900);
         
         // TODO this might not be the correct way to print it.
         logger.log(Level.INFO, "Created a new car: ", this);
@@ -37,7 +39,7 @@ public class Car {
      * 
      * @return 
      */
-    public Location getLocation() {
+    public Point getLocation() {
         logger.log(Level.INFO, "Location: ", this.location);
         return this.location;
     }
@@ -48,7 +50,7 @@ public class Car {
      * 
      * @param location 
      */
-    public void setLocation(Location location) {
+    public void setLocation(Point location) {
         this.location = location;
         logger.log(Level.INFO, "Location Updated: ", this.location);
     }
