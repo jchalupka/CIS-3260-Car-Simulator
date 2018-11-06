32f8d575f48553546cf9093abdb492cf4b49b0eb
Justin Braham <justin@Justins-MacBook-Pro.local>
2018-11-06 15:18:20 -0500

    Hit detection

--- src/carsimulator/CarSimulator.java
+++ src/carsimulator/CarSimulator.java
@@ -3,6 +3,7 @@ package carsimulator;
 import carsimulator.carui.Grid;
 import java.io.IOException;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
 
@@ -13,6 +14,7 @@ public class CarSimulator {
      */
     public static void main(String[] args) {
         // TODO turn this into a factory.
+        LogManager.getLogManager().reset();
         Gas gas = new Gas(100, 100);
         Direction direction = new Direction(0.0);
         
