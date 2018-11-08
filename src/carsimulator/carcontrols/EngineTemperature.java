7770d0865d6f7185cbca56050680d59abdcfe0c6
Kevin Pirabaharan <kevinpirabaharan@gmail.com>
2018-11-08 13:34:09 -0500

    Added Engine Temp Class

---
+++ src/carsimulator/carcontrols/EngineTemperature.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carsimulator.carcontrols;
+
+/**
+ *
+ * @author kevin
+ */
+public class EngineTemperature {
+     private double EngineTemperature;
+
+  /*
+  * Create the temperature class
+  * Default temperature is predefined temperature
+  */
+  public EngineTemperature(double obj)
+  {
+      this.EngineTemperature = obj;
+  }
+
+  /*
+  * Return the current engine temperature
+  @return temperature
+  */
+  public double getInternalTemp()
+  {
+      return this.EngineTemperature;
+  }
+
+  public void resetTemperature()
+  {
+      this.EngineTemperature = 0;
+  }
+
+  /*
+  * Increment the temperature
+  */
+  public void incrementTemp()
+  {
+      increaseTemp(0.05);
+  }
+
+  /*
+  * Increase engine temperature by a consisitent amount
+  * @param toAdd
+  */
+  public void increaseTemp(double toAdd)
+  {
+      this.EngineTemperature += toAdd;
+  }
+}
